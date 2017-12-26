27e026e7198a5fdc9e48890950338e894c4aca81
Keiga Sagae <sagae5.28rujeae@gmail.com>
2017-12-26 20:57:54 +0900

    show comment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,34 @@
 class PagesController < ApplicationController
+
+  #
+  # ||トップページを表示する||
+  #
   def index
   end
 
+  #
+  # ||コメントを読み込む||
+  #
   def read
     @reads = []
     # 全モデルをはく
-    Dir.glob(File.expand_path("./app/models/*.rb", Rails.root)).each do |f|
-      puts f
+    Dir.glob(File.expand_path("./app/controllers/*.rb", Rails.root)).each.with_index do |f, index|
+      puts index
       f = File.open(f)
       s = f.read  # 全て読み込む
       f.close
-      @reads << s
+      puts s
+      puts s.match(/\|\|(.+)\|\|/)
+      @reads << s.scan(/\|\|(.+)\|\|/)
+      @reads = @reads.flatten
     end
   end
+
+  #
+  # ||コメントを解析する||
+  #
+
+  #
+  # ||描画する||
+  #
 end
